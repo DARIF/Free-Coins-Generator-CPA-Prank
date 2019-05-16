747f0d223207bed6c422154edfc66c40db37c02c
Mohammed cha <40530012+Mohammedcha@users.noreply.github.com>
2019-05-16 21:29:58 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//      You must change the two pictures bg.jpg and header.png    //
+//      You can find them in a folder IMG    //
+
+$site_title = 'PubG Free Gold Coins';   	                            //     website title
+$site_description = 'Generate Gold Coins to your accounts PubG.';       //     Seo description 
+$site_keywords = 'PubG, free, coins, generator, games,';                //     Seo keywords
+$site_copyrights = 'Mohammed Cha © Copyright 2019';                     //     Footer Copyrights
+$site_img = 'https://raw.githubusercontent.com/Mohammedcha/Free-Coins-Generator-CPA-Prank/master/img/bg.jpg'; // Facebook poste sharing image
+$cpa_link = 'https://web.facebook.com/spidernichetool';                 //     CPA link here
+
+?>
